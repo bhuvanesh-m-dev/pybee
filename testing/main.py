c5d9a743ebdc3300c99cd960c2699e120bfdb11f
BHUVANESH M <bhuvaneshm.developer@gmail.com>
2025-07-29 18:26:32 +0530

    Update main.py

--- testing/main.py
+++ testing/main.py
@@ -1,1 +1,12 @@
+# main.py
+import tkinter as tk
+from app import PyBee
+
+if __name__ == "__main__":
+    """
+    Main entry point for the PyBee application.
+    """
+    root = tk.Tk()
+    app = PyBee(root)
+    root.mainloop()
 
